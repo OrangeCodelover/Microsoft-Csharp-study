29a63d2c433f196a7915300b82ae5e21a13241e4
OrangeCodelover <linenjimmy@gmail.com>
2025-04-01 16:10:35 +1300

    Program_8_4 complete

--- 04_Variable Data/CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ 04_Variable Data/CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42aeaf6efa71e0de8039347226d569c1c0063e15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed0d6f14ed338eb7d41a8f38fcac608be70432bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
